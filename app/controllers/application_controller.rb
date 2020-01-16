37f4b539945b4d8e82a81b7c7403b79ed68bf250
MarioDena <enrique.dn@outlook.com>
2020-01-16 11:35:51 -0600

    [Feature] Implemented devise, created models and assosiations for users and posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user!
 end
---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :content, :title, presence: true, length: { maximum: 140 }
+
+  default_scope -> { order(created_at: :desc) }
+end
