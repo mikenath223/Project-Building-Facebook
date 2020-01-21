5439e2d692d3c9951f68c1793077180e09aab97f
mike <michgoldennathan17@gmail.com>
2020-01-21 15:59:23 +0100

    [Feature] Create comments model and migrate

---
+++ db/migrate/20200121145427_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :comments, [:user_id, :post_id], unique: true
+  end
+end
