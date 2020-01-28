232ec65f4e2a7395a7673c197484779ac71476d3
MarioDena <enrique.dn@outlook.com>
2020-01-28 10:37:52 -0600

    [Feature] Implement base Friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,34 @@ class User < ApplicationRecord
   # belongs_to :user
 
   # validates :user_id, uniqueness: { scope: :post_id, message: 'You have already liked this post' }
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: :Friendship, foreign_key: :friend_id
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
