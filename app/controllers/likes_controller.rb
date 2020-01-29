72a165ec368b95dca06cded4849cbab417c4f770
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-01-29 17:27:13 +0100

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
-  before_action :set_likeable, only: %i[index create]
+  before_action :set_likeables, only: %i[index create]
 
   def index; end
 
@@ -25,14 +25,14 @@ class LikesController < ApplicationController
   private
 
   def already_liked?
-    !current_user.likes.find_by(likeable_id: like_params[:likeable_id], likeable_type: like_params[:likeable_type]).nil?
+    !current_user.likes.find_by(likeables_id: like_params[:likeables_id], likeables_type: like_params[:likeables_type]).nil?
   end
 
-  def set_likeable
-    @likeable = like_params[:likeable_type].constantize.find(like_params[:likeable_id])
+  def set_likeables
+    @likeable = like_params[:likeables_type].constantize.find(like_params[:likeables_id])
   end
 
   def like_params
-    params.require(:like).permit(:likeable_id, :likeable_type)
+    params.require(:like).permit(:likeables_id, :likeables_type)
   end
 end
