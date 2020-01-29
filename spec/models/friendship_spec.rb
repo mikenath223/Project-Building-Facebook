8c65a1cfcf366771df95e34f0696534970b1bef2
mike <michgoldennathan17@gmail.com>
2020-01-29 22:30:22 +0100

    [Test] Add  test for friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:friend_id).of_type(:integer) }
+  it { is_expected.to have_db_column(:confirmed).of_type(:boolean) }
+
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:friend) }
 end
