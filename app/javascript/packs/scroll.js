f4cf5c8722ed3553f62b09c8362374a1dfd39a25
mike <michgoldennathan17@gmail.com>
2020-02-13 14:21:55 +0100

    [Style] Include js for chat view on smaller screens

--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -1,3 +1,22 @@
-const chat = document.querySelector('.chat')
 
-document.body.onload(chat.scrollTo(0, chat.scrollHeight))
+// chat.scrollTo(0, chat.scrollHeight)
+
+const text = document.querySelector(".change-text");
+const chats = document.querySelectorAll("#chats");
+const headerFriend = document.querySelector(".head-friend");
+const friendsChat = document.querySelector(".friends-chat");
+const chatWrapper = document.querySelectorAll(".chat-wrapper");
+headerFriend.addEventListener('click', () => {
+  toggleClass(friendsChat, "friends-chat-prop");
+  chats.forEach((e) => {
+    e.setAttribute("style", "height: 120px; bottom: 0");
+  });
+  chatWrapper.forEach((e) => {
+    e.setAttribute("style", "top: initial; bottom: -140px, height: 120px");
+  });
+  text.textContent = "Click to hide...";
+});
+
+function toggleClass(elem, cssClass) {
+  elem.classList.toggle(cssClass);
+}
