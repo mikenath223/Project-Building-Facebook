63927282cee377363bd6ca5e33eba0f82556bf00
mike <michgoldennathan17@gmail.com>
2020-02-13 15:13:27 +0100

    [Feature] Add chat link to users views

--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -1,8 +1,10 @@
 
-// chat.scrollTo(0, chat.scrollHeight)
+const chats = document.querySelectorAll("#chats");
+chats.forEach((e) => {
+  e.scrollTo(0, e.scrollHeight);
+});
 
 const text = document.querySelector(".change-text");
-const chats = document.querySelectorAll("#chats");
 const headerFriend = document.querySelector(".head-friend");
 const friendsChat = document.querySelector(".friends-chat");
 const chatWrapper = document.querySelectorAll(".chat-wrapper");
