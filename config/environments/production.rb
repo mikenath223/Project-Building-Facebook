ae31d6465f88d916dc6b2d2b1daddbff50801a3d
mike <michgoldennathan17@gmail.com>
2020-02-14 14:20:38 +0100

    [Update] Refactor for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.web_socket_server_url = "wss://brookapp.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://brookapp.herokuapp.com', 'http://brookapp.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
